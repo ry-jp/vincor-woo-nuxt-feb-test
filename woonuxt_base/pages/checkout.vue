04d2827512b2159712dff6f126a5275b97b68462
ry-jp <ry_design@protonmail.com>
2024-02-28 16:44:04 -0600

    checkout disabled fix

--- woonuxt_base/pages/checkout.vue
+++ woonuxt_base/pages/checkout.vue
@@ -12,12 +12,7 @@ const stripeKey = runtimeConfig.public?.STRIPE_PUBLISHABLE_KEY;
 const stripeCardIsComplete = ref(false);
 
 const buttonText = ref(isProcessingOrder.value ? t('messages.general.processing') : t('messages.shop.checkoutButton'));
-const isCheckoutDisabled = computed(() => {
-  if (orderInput.value.paymentMethod === 'stripe') {
-    return isProcessingOrder.value || isUpdatingCart.value || !orderInput.value.paymentMethod || !stripeCardIsComplete.value;
-  }
-  return isProcessingOrder.value || isUpdatingCart.value || !orderInput.value.paymentMethod;
-});
+
 
 const emailRegex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,4}$');
 const isInvalidEmail = ref(false);
@@ -193,7 +188,7 @@ watch(
         <OrderSummary>
           <button
             class="flex items-center justify-center w-full gap-3 p-3 mt-4 font-semibold text-center text-white rounded-lg shadow-md bg-primary hover:bg-primary-dark disabled:cursor-not-allowed disabled:bg-gray-400"
-            :disabled="isCheckoutDisabled">
+            >
             {{ buttonText }}<LoadingIcon v-if="isProcessingOrder" color="#fff" size="18" />
           </button>
         </OrderSummary>
