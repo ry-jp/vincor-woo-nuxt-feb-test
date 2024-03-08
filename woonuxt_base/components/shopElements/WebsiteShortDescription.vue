9d01fd14e612540113bdbf5f2ada67391ebde76f
ry-jp <ry_design@protonmail.com>
2024-03-08 12:13:14 -0600

    index and short desc

--- woonuxt_base/components/shopElements/WebsiteShortDescription.vue
+++ woonuxt_base/components/shopElements/WebsiteShortDescription.vue
@@ -1,6 +1,8 @@
 <template>
-  <p class="mt-4 text-sm text-gray-700 xl:max-w-md">
-    WooNuxt is unmatched when it comes to performance and scalability. Reap the benefits of having a online store that out performs all of your competitors. You can edit components
-    to display your own information just like the one you're reading now.
+  <p class="mt-4 text-sm text-white w-5/6 xl:max-w-xl ">
+    Earth Station Antenna Specialists and Satellite Communication Product Distribution<br>
+    <span class=" italic">“We Work Harder For You”™ — a well-earned motto you can trust at Vincor™</span><br>
+
+Discuss your project needs with us Toll Free (CONUS): 1.888.284.6267; or outside the continental U.S. call 708.534.0008!
   </p>
 </template>
--- woonuxt_base/pages/index.vue
+++ woonuxt_base/pages/index.vue
@@ -1,6 +1,8 @@
 <script lang="ts" setup>
 const { data } = await useAsyncGql('getProductCategories', { first: 6 });
 const productCategories = data.value?.productCategories?.nodes || [];
+const { setProducts, updateProductList } = useProducts();
+const { data: featuredProductsData } = await useAsyncGql('getProducts', { first: 6 }); 
 
 useSeoMeta({
   title: `Home`,
@@ -19,16 +21,36 @@ useSeoMeta({
 
       <h2 class="text-lg font-semibold md:text-2xl">Featured Brands:</h2>
     <div class="container flex flex-wrap items-center justify-center gap-8 my-16 text-center brand lg:justify-between">
+    
       <NuxtImg src="/images/brands/norsat.png" alt="Brand 1" width="200" height="35" />
-      <NuxtImg format="png" src="/images/brands/eti.png" alt="Brand 2" width="80" height="30" />
+      <NuxtImg src="/images/brands/eti.png" alt="Brand 2" width="80" height="30" />
       <NuxtImg src="/images/brands/andrew.png" alt="Brand 3" width="175" height="48" />
       <NuxtImg src="/images/brands/venture.png" alt="Brand 4" width="200" height="30" />
       <NuxtImg src="/images/brands/quintech.png" alt="Brand 5" width="200" height="44" />
       <NuxtImg src="/images/brands/dh.png" alt="Brand 6" width="65" height="40" />
 
+
 </div>
     </div>
+    <section class="container my-16">
+      <div class="flex items-end justify-between">
+        <h2 class="text-lg font-semibold md:text-2xl">{{ $t('messages.shop.shopByCategory') }}</h2>
+        <NuxtLink class="text-primary" to="/categories">{{ $t('messages.general.viewAll') }}</NuxtLink>
+      </div>
+      <div class="grid justify-center grid-cols-2 gap-4 mt-8 md:grid-cols-3 lg:grid-cols-6">
+        <CategoryCard v-for="(category, i) in productCategories" :key="i" class="w-full" :node="category" />
+      </div>
 
+    </section>
+
+
+    <section class="container my-16">
+
+    </section>
+
+
+
+    
   </main>
 </template>
 
